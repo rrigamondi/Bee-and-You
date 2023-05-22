5b52bc3e76f6d7fb0bee937cf49b69ec9eccd752
rrigamondi <riccardo1.rigamondi@mail.polimi.it>
2023-05-22 18:26:43 +0200

    rough seed planting implementation

--- Unity_BeeAndYou/Assets/Scripts/BeeAndYouScripts/UserActions/SeedBehavior.cs
+++ Unity_BeeAndYou/Assets/Scripts/BeeAndYouScripts/UserActions/SeedBehavior.cs
@@ -4,42 +4,36 @@ using UnityEngine;
 
 public class SeedBehavior : MonoBehaviour
 {
-    public bool seedActive = false;
+    bool seedActive = false;
     public GameObject seed;
+    public GameObject seedModel;
     public GameObject seedPrefab;
     public GameObject flowerPrefab;
 
     public Vector3 seedSpawnPos;
-    public Vector3 seedTrackedPos;
+    Vector3 seedTrackedPos;
+
+    //public Vector3 seedSpawnPos = (55,33,55);
+    //Vector3 seedTrackedPos = (0,50,0);
 
     //temporary
-    public int plantingHeight = 0;
+    public float plantingHeight = 33.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-      // !!!!!!! temporary, to substitute with digging gesture recognition
-      if (seedActive == true)
-      {
-        seedTrackedPos = seed.transform.position;
-        if (seedTrackedPos.x <= plantingHeight) //should also check if user is holding it
-        {
-          SeedPlanting();
-        }
-      }
+      seedSpawnPos = new Vector3(55.0f,33.0f,55.0f);
+      seedTrackedPos = new Vector3(55.0f,33.0f,55.0f);
+      Destroy(seed);
+      seedActive = false;
     }
 
     public void SeedSpawn()
     {
       //spawn the seed, get the rigidbody, set active
-      GameObject seed = Instantiate(seedPrefab, seedSpawnPos, Quaternion.identity);
-      GameObject seedModel = seed.transform.GetChild(0).gameObject;
+      seed = Instantiate(seedPrefab, seedSpawnPos, Quaternion.identity);
+      seedModel = seed.transform.GetChild(0).gameObject;
+      Debug.Log("SeedSpawn "+seedActive + " / " + seed);
 
       seedModel.GetComponent<Rigidbody>().useGravity = false;
       // !! need to reactivate gravity when user grabs it
@@ -50,13 +44,40 @@ public class SeedBehavior : MonoBehaviour
 
     public void SeedPlanting()
     {
+      Debug.Log("SeedPlanting "+seedActive);
       // track current vector3 position of the seed, destroy it and instantiate a flower instead
-      seedTrackedPos = seed.transform.position;
+      //seedTrackedPos = seed.transform.position;
       Destroy(seed);
-
+      seedTrackedPos.y -= 0.1f;
       // !! will need to differentiate flowers with multiple prefabs, could have different seeds to if we have enough time
       Instantiate(flowerPrefab, seedTrackedPos, Quaternion.identity);
 
       seedActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // !!!!!!! temporary, to substitute with digging gesture recognition
+      if (seedActive == false)
+      {
+        Debug.Log("Update and spawn "+seedActive);
+        SeedSpawn();
+      }
+      else
+      {
+        //GetComponent<Rigidbody>().position
+
+        Debug.Log("Update and plant "+seedActive + " / " + seed);
+        seedTrackedPos = seedModel.GetComponent<Rigidbody>().position;
+        Debug.Log(seedTrackedPos);
+
+        Debug.Log(seedTrackedPos.y + " / " + plantingHeight);
+        if (seedTrackedPos.y <= plantingHeight) //should also check if user is holding it
+        {
+          Debug.Log("Start SeedPlanting");
+          SeedPlanting();
+        }
+      }
     }
 }
