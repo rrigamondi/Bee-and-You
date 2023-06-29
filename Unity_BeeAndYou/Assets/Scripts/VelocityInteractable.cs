178d7d0d5cfbda02f6e4694fee58201f6d364215
moleculecr <rui1.chen@mail.polimi.it>
2023-06-29 11:19:56 +0200

    commit

---
+++ Unity_BeeAndYou/Assets/Scripts/VelocityInteractable.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VelocityInteractable : XRGrabInteractable
+{
+    private ControllerVelocity controllerVelocity = null;
+    private MeshRenderer meshRenderer = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    protected internal virtual void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        controllerVelocity = args.interactor.GetComponent<ControllerVelocity>();
+    }
+
+    protected internal virtual void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        controllerVelocity = null;
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+
+        if (isSelected)
+        {
+            if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+                UpdateColorUsingVelocity();
+        }
+    }
+
+    private void UpdateColorUsingVelocity()
+    {
+        Vector3 velocity = controllerVelocity ? controllerVelocity.Velocity : Vector3.zero;
+        Color color = new Color(velocity.x, velocity.y, velocity.z);
+        meshRenderer.material.color = color;
+    }
+}
