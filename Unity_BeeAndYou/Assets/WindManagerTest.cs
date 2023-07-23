654db5350aaaaf49362cc03085953929f51de500
Riccardo Rigamondi <50756749+rrigamondi@users.noreply.github.com>
2023-07-23 23:49:34 +0200

    Wind Zone

---
+++ Unity_BeeAndYou/Assets/WindManagerTest.cs
@@ -0,0 +1,21 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using VisualDesignCafe.Nature;
+//
+// public class WindManagerTest : MonoBehaviour
+// {
+//     public GameObject wind;
+//
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         WindSettings windSettings = wind.GetComponent<WindSettings>().get_Calm;
+//     }
+//
+//     // Update is called once per frame
+//     void Update()
+//     {
+//
+//     }
+// }
