61ba83cdd3062f6177cf5973df1340753d1c118e
Riccardo Rigamondi <50756749+rrigamondi@users.noreply.github.com>
2023-07-23 20:08:25 +0200

    Teleportation method thumbstick

--- Unity_BeeAndYou/Assets/Scripts/BeeAndYouScripts/UserActions/SeedGrabbed.cs
+++ Unity_BeeAndYou/Assets/Scripts/BeeAndYouScripts/UserActions/SeedGrabbed.cs
@@ -8,9 +8,14 @@ public class SeedGrabbed : MonoBehaviour
     public GameObject notification;
 
     public bool seedIsGrabbed = false;
-    bool firstGrab = true;
+    public bool firstGrab = true;
 
     public GameObject eventSys;
+
+    public float notificationFirstAnimationSpeed = 1.0f;
+    public Vector3 notificationEndPosition;
+
+    bool animationActive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +25,14 @@ public class SeedGrabbed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      if(animationActive)
+      {
+        Vector3 direction = (notificationEndPosition - notification.transform.position).normalized;
+        if (Vector3.Distance(notification.transform.position, notificationEndPosition) >= 0.1f)
+        {
+          notification.transform.position += direction * notificationFirstAnimationSpeed * Time.deltaTime;
+        }
+      }
 
     }
 
@@ -27,19 +40,29 @@ public class SeedGrabbed : MonoBehaviour
     {
       seedIsGrabbed = true;
       GetComponent<Rigidbody>().useGravity = true;
-      notification.SetActive(false);
 
       transform.Rotate(-180,0,0);
 
-      if (firstGrab)
+      if (firstGrab == false)
+      {
+        notification.SetActive(false);
+      }
+      else
       {
         eventSys.GetComponent<SeedBehavior>().FirstGrab();
-        firstGrab = false;
+
+        animationActive = true;
+        // var originalPos = transform.TransformPoint(notification.GetComponent<Transform>().position);
+        // var step =  notificationFirstAnimationSpeed * Time.deltaTime;
+        // transform.TransformPoint(notificationEndPosition);
+        // notification.GetComponent<Transform>().position = Vector3.MoveTowards(originalPos, notificationEndPosition, step);
       }
+      //Debug.Log(firstGrab + " / " + notification.GetComponent<Transform>().position);
     }
 
     public void Ungrabbed()
     {
       seedIsGrabbed = false;
+      firstGrab = false;
     }
 }
